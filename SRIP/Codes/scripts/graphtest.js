4552cabfc6cf3e568fe0ad21c949bcb0b2c96bcf
Rohan Nawathe <rohannawathe@hotmail.com>
2019-06-12 02:26:30 +0530

    Repaired basic graphs

--- SRIP/Codes/scripts/graphtest.js
+++ SRIP/Codes/scripts/graphtest.js
@@ -1,8 +1,25 @@
-function makegraph() {
-//    let forces = new Map();
-//    {
-//        0 : -5,
-//        2 : 10,
-//        4 : -5
-//    }; // Location of force : Value of Force
+function makegraph(vals) {
+    var values_x = vals['values_x'];
+    var shearForce = vals['shearForce'];
+    var bendMoment = vals['bendMoment'];
+    Plotly.plot('concSFD',[{
+        x : values_x,
+        y : shearForce
+    }]);
+
+    Plotly.plot('concBMD',[{
+        x : values_x,
+        y : bendMoment
+    }]);
+
+    return;
+}
+
+function testgraph(){
+    var loadparams = new Object();
+    loadparams.positions = [50];
+    loadparams.weights = [5];
+    var beamLength = 100;
+    var vals = calcSFBMConcMid(beamLength, loadparams);
+    makegraph(vals);
 }
